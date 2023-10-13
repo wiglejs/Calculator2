118f2ab0fb1fdddc9cf86a8605c2c99d53768679
wiglejs <jamesbusiness43@gmail.com>
2023-10-12 20:30:48 -0600

    Added all buttons, Names, and resized

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    partial class Form1
+    partial class Cal_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Dec = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Pow = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Sum = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Mult = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Sqrt = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Devi = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Num0
+            // 
+            this.Num0.Location = new System.Drawing.Point(12, 373);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(70, 65);
+            this.Num0.TabIndex = 0;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            // 
+            // Dec
+            // 
+            this.Dec.Location = new System.Drawing.Point(93, 373);
+            this.Dec.Name = "Dec";
+            this.Dec.Size = new System.Drawing.Size(70, 65);
+            this.Dec.TabIndex = 1;
+            this.Dec.Text = ".";
+            this.Dec.UseVisualStyleBackColor = true;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(174, 373);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(70, 65);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "C";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(255, 373);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(155, 65);
+            this.Calculate.TabIndex = 3;
+            this.Calculate.Text = "=";
+            this.Calculate.UseVisualStyleBackColor = true;
+            // 
+            // Pow
+            // 
+            this.Pow.Location = new System.Drawing.Point(255, 302);
+            this.Pow.Name = "Pow";
+            this.Pow.Size = new System.Drawing.Size(70, 65);
+            this.Pow.TabIndex = 7;
+            this.Pow.Text = "Pow 2";
+            this.Pow.UseVisualStyleBackColor = true;
+            // 
+            // Num9
+            // 
+            this.Num9.Location = new System.Drawing.Point(174, 302);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(70, 65);
+            this.Num9.TabIndex = 6;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            // 
+            // Num8
+            // 
+            this.Num8.Location = new System.Drawing.Point(93, 302);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(70, 65);
+            this.Num8.TabIndex = 5;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            // 
+            // Num7
+            // 
+            this.Num7.Location = new System.Drawing.Point(12, 302);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(70, 65);
+            this.Num7.TabIndex = 4;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            // 
+            // Sum
+            // 
+            this.Sum.Location = new System.Drawing.Point(255, 231);
+            this.Sum.Name = "Sum";
+            this.Sum.Size = new System.Drawing.Size(70, 65);
+            this.Sum.TabIndex = 11;
+            this.Sum.Text = "+";
+            this.Sum.UseVisualStyleBackColor = true;
+            // 
+            // Num6
+            // 
+            this.Num6.Location = new System.Drawing.Point(174, 231);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(70, 65);
+            this.Num6.TabIndex = 10;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            // 
+            // Num5
+            // 
+            this.Num5.Location = new System.Drawing.Point(93, 231);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(70, 65);
+            this.Num5.TabIndex = 9;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            // 
+            // Num4
+            // 
+            this.Num4.Location = new System.Drawing.Point(12, 231);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(70, 65);
+            this.Num4.TabIndex = 8;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            // 
+            // Mult
+            // 
+            this.Mult.Location = new System.Drawing.Point(255, 160);
+            this.Mult.Name = "Mult";
+            this.Mult.Size = new System.Drawing.Size(70, 65);
+            this.Mult.TabIndex = 15;
+            this.Mult.Text = "X";
+            this.Mult.UseVisualStyleBackColor = true;
+            // 
+            // Num3
+            // 
+            this.Num3.Location = new System.Drawing.Point(174, 160);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(70, 65);
+            this.Num3.TabIndex = 14;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(93, 160);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(70, 65);
+            this.Num2.TabIndex = 13;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(12, 160);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(70, 65);
+            this.Num1.TabIndex = 12;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            // 
+            // Sqrt
+            // 
+            this.Sqrt.Location = new System.Drawing.Point(340, 302);
+            this.Sqrt.Name = "Sqrt";
+            this.Sqrt.Size = new System.Drawing.Size(70, 65);
+            this.Sqrt.TabIndex = 16;
+            this.Sqrt.Text = "Sqrt";
+            this.Sqrt.UseVisualStyleBackColor = true;
+            // 
+            // Sub
+            // 
+            this.Sub.Location = new System.Drawing.Point(340, 231);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(70, 65);
+            this.Sub.TabIndex = 17;
+            this.Sub.Text = "-";
+            this.Sub.UseVisualStyleBackColor = true;
+            // 
+            // Devi
+            // 
+            this.Devi.Location = new System.Drawing.Point(340, 160);
+            this.Devi.Name = "Devi";
+            this.Devi.Size = new System.Drawing.Size(70, 65);
+            this.Devi.TabIndex = 18;
+            this.Devi.Text = "/";
+            this.Devi.UseVisualStyleBackColor = true;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 117);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(398, 20);
+            this.Output.TabIndex = 19;
+            // 
+            // Cal_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(423, 491);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Devi);
+            this.Controls.Add(this.Sub);
+            this.Controls.Add(this.Sqrt);
+            this.Controls.Add(this.Mult);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.Sum);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Pow);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Dec);
+            this.Controls.Add(this.Num0);
+            this.Name = "Cal_Form";
+            this.Text = "My Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Dec;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button Pow;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Sum;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Mult;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Sqrt;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Devi;
+        private System.Windows.Forms.TextBox Output;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class Cal_Form : Form
     {
-        public Form1()
+        public Cal_Form()
         {
             InitializeComponent();
         }
